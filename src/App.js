cc687f93f0300ff9834f1a646cedd0d13040e110
manny <mangostar.302@gmail.com>
2023-11-28 22:35:24 -0500

    fixing routing issues

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from './Home';
+import About from './About';
+import Services from './Services';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <nav className="navbar">
-        <div className="navbar-container">
-          <img src={logo} className="navbar-logo" alt="logo" />
-          <ul className="navbar-links">
-            <li><a href="/">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
-      </nav>
+      <div className="App">
+        <nav className="navbar">
+          <div className="navbar-container">
+            <img src={logo} className="navbar-logo" alt="logo" />
+            <ul className="navbar-links">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/services">Services</Link></li>
+              {/* Add more navbar links here if needed */}
+            </ul>
+          </div>
+        </nav>
 
-      <Route path="/" exact component={Home} />
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
